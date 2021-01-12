2d25b9335256963bd66d572480415761f1f31da3
jinof <jinof@foxmail.com>
2021-01-12 23:09:04 +0800

    :art: 抽离板荷载计算代码

--- board/board.go
+++ board/board.go
@@ -2,51 +2,35 @@ package board
 
 import (
 	"fmt"
-	"github.com/Jinof/concrete/pkg"
 	"log"
+	"math"
+
+	"github.com/Jinof/concrete/pkg"
+)
+
+var (
+	g       float64 // g 永久荷载设计值
+	q       float64 // q 可变荷载设计值
+	sumLoad float64 // sumLoad 荷载总设计值
+	sumCG   float64 // 板的永久荷载标准值小计
 )
 
 // CalBoard calculate board
 func CalBoard() {
+	// l01 边跨, l02 中间跨
 	// l01 = 1970 mm, l02 = 2000 mm
 	l01 := 1.97
-	l02 := 2
-
-	// 板的永久荷载标准值
-	// smsG 水磨石 0.65 KN/m^2
-	// hntbG 70mm 钢筋混凝土板 0.07 * 25 KN/m^2
-	// sjG 20 mm 混合砂浆 0.02 * 17 KN/m^2
-	smsConsistantLoad := 0.65
-	hntbConsistantLoad := 0.07 * 25
-	sjConsistantLoad := 0.02 * 17
-	sumCG := smsConsistantLoad + hntbConsistantLoad + sjConsistantLoad
-
-	// 板可变荷载标准值 4.5 KN/m^2
-	dynamicLoad := 4.5
-
-	// 永久荷载分项系数 1.3
-	// 可变荷载分项系数 1.5
-	// g 永久荷载设计值
-	g := sumCG * 1.3
-	// q 可变荷载设计值
-	q := dynamicLoad * 1.5
-	// sumLoad 荷载总设计值
-	sumLoad := g + q
-	// sumLoad == 10.312
-	// 取sumLoad = 10.3
-	t := sumLoad
-	sumLoad = 10.3
-	fmt.Println("sumLoad 荷载总设计值", t, "实际取", sumLoad)
+	l02 := 2.0
 
 	var gq float64
 	gq = sumLoad
-	//
-	MA := -gq * float64(l01*l01) / 16
-	M1 := gq * float64(l01*l01) / 14
-	MB := -gq * float64(l01*l01) / 11
-	MC := -gq * float64(l02*l02) / 14
+
+	MA := -gq * math.Pow(l01, 2) / 16
+	M1 := gq * math.Pow(l01, 2) / 14
+	MB := -gq * math.Pow(l01, 2) / 11
+	MC := -gq * math.Pow(l02, 2) / 14
 	// M2 = M3
-	M2 := gq * float64(l02*l02) / 16
+	M2 := gq * math.Pow(l02, 2) / 16
 
 	Printer(pkg.A, MA)
 	Printer(pkg.FIRST, M1)
@@ -83,4 +67,40 @@ func CalReinforcement(h float64, As float64, location string) (err error) {
 		log.Fatal("[CalReinforcement]", err)
 	}
 	return
+}
+
+// CalLoad calculate the load of board
+func CalLoad() {
+	// 板的永久荷载标准值
+	// smsG 水磨石 0.65 KN/m^2
+	// hntbG 70mm 钢筋混凝土板 0.07 * 25 KN/m^2
+	// sjG 20 mm 混合砂浆 0.02 * 17 KN/m^2
+	smsConsistantLoad := 0.65
+	hntbConsistantLoad := 0.07 * 25
+	sjConsistantLoad := 0.02 * 17
+	sumCG = smsConsistantLoad + hntbConsistantLoad + sjConsistantLoad
+
+	// 板可变荷载标准值 4.5 KN/m^2
+	dynamicLoad := 4.5
+
+	// 永久荷载分项系数 1.3
+	// 可变荷载分项系数 1.5
+	g = sumCG * 1.3
+	q := dynamicLoad * 1.5
+	sumLoad = g + q
+	// sumLoad == 10.312
+	// 取sumLoad = 10.3
+	t := sumLoad
+	sumLoad = 10.3
+
+	fmt.Println("sumLoad 荷载总设计值", t, "实际取", sumLoad)
+}
+
+func init() {
+	CalLoad()
+}
+
+// GetG return g
+func GetG() float64 {
+	return g
 }
